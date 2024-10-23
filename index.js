a93fd4ae027c479432d695dba4691d1e861ad4a2
roshan-appsndevices <roshan@appsndevices.in>
2024-10-23 11:58:58 +0530

    schema change

--- index.js
+++ index.js
@@ -48,10 +48,23 @@ app.get("/", async (req, res) => {
 // Create New Expense
 app.post("/saveUser", async (req, res) => {
   try {
+    // Check if a user with the same emailId already exists
+    const existingUser = await Expense.findOne({ emailId: req.body.emailId });
+
+    if (existingUser) {
+      // If user with the same emailId exists, return an error message
+      return res
+        .status(200)
+        .json({ code: 200, message: "User with this email already exists" });
+    }
+
+    // If no user with the same emailId exists, create a new user
     const newExpense = new Expense(req.body);
     await newExpense.save();
+
     res.status(200).json({ code: 200, message: "User saved successfully" });
   } catch (err) {
+    console.error(err);
     res.status(500).json({ code: 500, message: "Error saving user" });
   }
 });
@@ -72,13 +85,11 @@ app.put("/editExpense/:expId", async (req, res) => {
       return res.status(404).json({ code: 404, message: "Expense not found" });
     }
 
-    res
-      .status(200)
-      .json({
-        code: 200,
-        message: "Expense updated successfully",
-        updatedExpense,
-      });
+    res.status(200).json({
+      code: 200,
+      message: "Expense updated successfully",
+      updatedExpense,
+    });
   } catch (err) {
     res.status(500).json({ code: 500, message: err.message });
   }
